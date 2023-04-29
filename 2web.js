f6fbfbab7994a96bb183cc05677e6d633d1494f6
dude56987 <dude56987@users.noreply.github.com>
2023-04-29 19:38:22 -0400

    Fixed strange menu behavior during window resize
    - Fixed window resizing and device screen rotation making the menu buttons appear/disappear unexpectedly
    - Removed mixed indentation and converted all spaces to tabs

--- 2web.js
+++ 2web.js
@@ -1,205 +1,216 @@
-//-----------------------------------------------------------------------------
-function toggleVisibleClass( visibleClass ){
-  var elementArray = document.getElementsByClassName( visibleClass );
-  if (elementArray[0].hidden == false){
-    // make hidden items visible
-    var newVisibility = true;
-  } else {
-    // make visible items hidden
-    var newVisibility = false;
-  }
-  //console.log(elementArray);
-  for (var index=0; index < elementArray.length;index++){
-   //console.log(elementArray[index]);
-   elementArray[index].hidden = newVisibility;
-  }
-  return true
-}
-//-----------------------------------------------------------------------------
+////////////////////////////////////////////////////////////////////////////////
+// 2web javascript library
+// Copyright (C) 2023  Carl J Smith
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+////////////////////////////////////////////////////////////////////////////////
 function hideVisibleClass( visibleClass ){
 	var elementArray = document.getElementsByClassName( visibleClass );
-	// make hidden items visible
-	var newVisibility = true;
-  for (var index=0; index < elementArray.length;index++){
-   elementArray[index].hidden = newVisibility;
-  }
-  return true
-}
-//-----------------------------------------------------------------------------
-function setHeaderStartState(){
-	if (window.innerWidth < window.innerHeight || window.innerWidth < 800 ) {
-		console.log("Phone Mode")
-		// this is the phone portrat mode
-		toggleVisibleClass("headerButtons");
-	}else{
-		// this is the desktop mode
-		console.log("Desktop Mode")
+	for (var index=0; index < elementArray.length;index++){
+		elementArray[index].hidden = true;
 	}
+
+	return true;
+}
+////////////////////////////////////////////////////////////////////////////////
+function showVisibleClass( visibleClass ){
+	var elementArray = document.getElementsByClassName( visibleClass );
+	for (var index=0; index < elementArray.length;index++){
+		elementArray[index].hidden = false;
+	}
+
+	return true;
+}
+////////////////////////////////////////////////////////////////////////////////
+function toggleVisibleClass( visibleClass ){
+	var elementArray = document.getElementsByClassName( visibleClass );
+	for (var index=0; index < elementArray.length;index++){
+		if (elementArray[index].hidden === false){
+			// make hidden items visible
+			hideVisibleClass( visibleClass );
+		} else {
+			// make visible items hidden
+			showVisibleClass( visibleClass );
+		}
+	}
+	return true
+}
+////////////////////////////////////////////////////////////////////////////////
+function setHeaderState(){
+	if (window.innerWidth < window.innerHeight || window.innerWidth < 800 ) {
+		hideVisibleClass("headerButtons");
+	}else{
+		showVisibleClass("headerButtons");
+	}
+}
+////////////////////////////////////////////////////////////////////////////////
+function setHeaderStartState(){
+	// run the state setup
+	setHeaderState();
+	// set a resize event for updating the header state on window resize or phone rotate
+	window.addEventListener('resize', function(event){
+		// set the header state based on the screen size
+		// if the window is in portrat mode
+		setHeaderState();
+	});
 }
 //-----------------------------------------------------------------------------
 function filterByClass(className,searchText){
-  var filter = searchText;
-  filter = filter.toLowerCase();
-  console.log(filter);//debug
+	var filter = searchText;
+	filter = filter.toLowerCase();
 
-  // find all elements of htmlClass
-  var elements = document.getElementsByClassName(className);
+	// find all elements of htmlClass
+	var elements = document.getElementsByClassName(className);
 
-  console.log(elements);//debug
-  console.log(elements.toString());//debug
 
-  var tempContent = "";
-  // show all elements containing the search term
-  for (elementIndex in elements) {
-    console.log(elements[elementIndex]);//debug
+	var tempContent = "";
+	// show all elements containing the search term
+	for (elementIndex in elements) {
 
-    tempContent = elements[elementIndex].textContent;
-    tempContent.toLowerCase();
+		tempContent = elements[elementIndex].textContent;
+		tempContent.toLowerCase();
 
-    console.log(tempContent);//debug
 
-    // hide element if it contains the search term
-    if ( tempContent.search(filter) != -1 ) {
-      console.log("Found matching element");
-      // show elements that match the search
-      elements[elementIndex].style.display = "inline-block";
-    } else {
-      console.log("Found non-matching element");
-      // hide elements that do not contain the filter phrase
-      elements[elementIndex].style.display = "none";
-    }
-  }
+		// hide element if it contains the search term
+		if ( tempContent.search(filter) != -1 ) {
+			// show elements that match the search
+			elements[elementIndex].style.display = "inline-block";
+		} else {
+			// hide elements that do not contain the filter phrase
+			elements[elementIndex].style.display = "none";
+		}
+	}
 }
 //-----------------------------------------------------------------------------
 function filter(className){
-  // read searchbox text to create the filter
-  var searchBoxElement = document.getElementById('searchBox');
-  console.log(searchBoxElement);//debug
-  console.log(searchBoxElement.value);//debug
+	// read searchbox text to create the filter
+	var searchBoxElement = document.getElementById('searchBox');
 
-  //var filter = searchBoxElement.innerHTML;
-  var filter = searchBoxElement.value;
-  filter = filter.toLowerCase();
-  console.log(filter);//debug
+	//var filter = searchBoxElement.innerHTML;
+	var filter = searchBoxElement.value;
+	filter = filter.toLowerCase();
 
-  // find all elements of htmlClass
-  var elements = document.getElementsByClassName(className);
+	// find all elements of htmlClass
+	var elements = document.getElementsByClassName(className);
 
-  console.log(elements);//debug
-  console.log(elements.toString());//debug
+	var tempContent = "";
+	// show all elements containing the search term
+	for (elementIndex in elements) {
 
-  var tempContent = "";
-  // show all elements containing the search term
-  for (elementIndex in elements) {
-    console.log(elements[elementIndex]);//debug
+		tempContent = elements[elementIndex].textContent;
+		tempContent = tempContent.toLowerCase();
 
-    tempContent = elements[elementIndex].textContent;
-    tempContent = tempContent.toLowerCase();
-
-    console.log(tempContent);//debug
-
-    // hide element if it contains the search term
-    if ( tempContent.search(filter) != -1 ) {
-      console.log("Found matching element");
-      // show elements that match the search
-      elements[elementIndex].style.display = "inline-block";
-    } else {
-      console.log("Found non-matching element");
-      // hide elements that do not contain the filter phrase
-      elements[elementIndex].style.display = "none";
-    }
-  }
+		// hide element if it contains the search term
+		if ( tempContent.search(filter) != -1 ) {
+			// show elements that match the search
+			elements[elementIndex].style.display = "inline-block";
+		} else {
+			// hide elements that do not contain the filter phrase
+			elements[elementIndex].style.display = "none";
+		}
+	}
 }
 ////////////////////////////////////////////////////////////////////////////////
 function playPause(){
-  var video = document.getElementById("video");
-  var playButton = document.getElementById("playButton");
-  var pauseButton = document.getElementById("pauseButton");
-  if(video.paused){
-    video.play();
-    playButton.style.display = 'none';
-    pauseButton.style.display = 'inline-block';
-  }else{
-    video.pause();
-    playButton.style.display = 'inline-block';
-    pauseButton.style.display = 'none';
-  }
+	var video = document.getElementById("video");
+	var playButton = document.getElementById("playButton");
+	var pauseButton = document.getElementById("pauseButton");
+	if(video.paused){
+		video.play();
+		playButton.style.display = 'none';
+		pauseButton.style.display = 'inline-block';
+	}else{
+		video.pause();
+		playButton.style.display = 'inline-block';
+		pauseButton.style.display = 'none';
+	}
 	return false;
 }
 ////////////////////////////////////////////////////////////////////////////////
 function forcePlay(){
-  var video = document.getElementById("video");
-  var playButton = document.getElementById("playButton");
-  var pauseButton = document.getElementById("pauseButton");
-  video.play();
-  playButton.style.display = 'none';
-  pauseButton.style.display = 'inline-block';
+	var video = document.getElementById("video");
+	var playButton = document.getElementById("playButton");
+	var pauseButton = document.getElementById("pauseButton");
+	video.play();
+	playButton.style.display = 'none';
+	pauseButton.style.display = 'inline-block';
 	return false;
 }
 ////////////////////////////////////////////////////////////////////////////////
 function volumeUp(){
-  var tempVolume = document.getElementById("video").volume;
-  if ( (tempVolume + 0.05) > 1 ){
-    // dont let volume go below zero
-    tempVolume = 1;
-    document.getElementById("video").volume = 1;
-  } else {
-    document.getElementById("video").volume += 0.05;
-  }
-  tempVolume = Math.floor(tempVolume * 100);
-  if (tempVolume < 10){
-    var volumeString = "&nbsp;&nbsp;"+String(tempVolume);
-  }else if (tempVolume < 100){
-    var volumeString = "&nbsp;"+String(tempVolume);
-  } else {
-    var volumeString = String(tempVolume);
-  }
-  document.getElementById("currentVolume").innerHTML=volumeString;
+	var tempVolume = document.getElementById("video").volume;
+	if ( (tempVolume + 0.05) > 1 ){
+		// dont let volume go below zero
+		tempVolume = 1;
+		document.getElementById("video").volume = 1;
+	} else {
+		document.getElementById("video").volume += 0.05;
+	}
+	tempVolume = Math.floor(tempVolume * 100);
+	if (tempVolume < 10){
+		var volumeString = "&nbsp;&nbsp;"+String(tempVolume);
+	}else if (tempVolume < 100){
+		var volumeString = "&nbsp;"+String(tempVolume);
+	} else {
+		var volumeString = String(tempVolume);
+	}
+	document.getElementById("currentVolume").innerHTML=volumeString;
 	return false;
 }
 ////////////////////////////////////////////////////////////////////////////////
 function volumeDown(){
-  var tempVolume = document.getElementById("video").volume;
-  if ( (tempVolume - 0.05) < 0 ){
-    // dont let volume go above one
-    tempVolume = 0;
-    document.getElementById("video").volume = 0;
-  } else {
-    document.getElementById("video").volume -= 0.05;
-  }
-  tempVolume = Math.floor(tempVolume * 100);
-  if (tempVolume < 10){
-    var volumeString = "&nbsp;&nbsp;"+String(tempVolume);
-  }else if (tempVolume < 100){
-    var volumeString = "&nbsp;"+String(tempVolume);
-  } else {
-    var volumeString = String(tempVolume);
-  }
-  document.getElementById("currentVolume").innerHTML=volumeString;
+	var tempVolume = document.getElementById("video").volume;
+	if ( (tempVolume - 0.05) < 0 ){
+		// dont let volume go above one
+		tempVolume = 0;
+		document.getElementById("video").volume = 0;
+	} else {
+		document.getElementById("video").volume -= 0.05;
+	}
+	tempVolume = Math.floor(tempVolume * 100);
+	if (tempVolume < 10){
+		var volumeString = "&nbsp;&nbsp;"+String(tempVolume);
+	}else if (tempVolume < 100){
+		var volumeString = "&nbsp;"+String(tempVolume);
+	} else {
+		var volumeString = String(tempVolume);
+	}
+	document.getElementById("currentVolume").innerHTML=volumeString;
 	return false;
 }
 ////////////////////////////////////////////////////////////////////////////////
 function muteUnMute(){
-  var video = document.getElementById("video");
-  var muteButton = document.getElementById("muteButton");
-  var unMuteButton = document.getElementById("unMuteButton");
-  if(video.muted){
-    video.muted = true;
-    muteButton.style.display = 'none';
-    unMuteButton.style.display = 'inline-block';
-  }else{
-    video.muted = false;
-    muteButton.style.display = 'inline-block';
-    unMuteButton.style.display = 'none';
-  }
+	var video = document.getElementById("video");
+	var muteButton = document.getElementById("muteButton");
+	var unMuteButton = document.getElementById("unMuteButton");
+	if(video.muted){
+		video.muted = true;
+		muteButton.style.display = 'none';
+		unMuteButton.style.display = 'inline-block';
+	}else{
+		video.muted = false;
+		muteButton.style.display = 'inline-block';
+		unMuteButton.style.display = 'none';
+	}
 	return false;
 }
 ////////////////////////////////////////////////////////////////////////////////
 function showControls(){
-  var video = document.getElementById("video");
-  var showControls = document.getElementById("showControls");
-  var hideControls = document.getElementById("hideControls");
-  // show controls for video
+	var video = document.getElementById("video");
+	var showControls = document.getElementById("showControls");
+	var hideControls = document.getElementById("hideControls");
+	// show controls for video
 	showControls.style.display = 'none';
 	hideControls.style.display = 'inline-block';
 	video.setAttribute("controls","controls");
@@ -207,10 +218,10 @@ function showControls(){
 }
 ////////////////////////////////////////////////////////////////////////////////
 function hideControls(){
-  var video = document.getElementById("video");
-  var showControls = document.getElementById("showControls");
-  var hideControls = document.getElementById("hideControls");
-  // show controls for video
+	var video = document.getElementById("video");
+	var showControls = document.getElementById("showControls");
+	var hideControls = document.getElementById("hideControls");
+	// show controls for video
 	showControls.style.display = 'inline-block';
 	hideControls.style.display = 'none';
 	video.removeAttribute("controls");
@@ -222,7 +233,7 @@ function reloadVideo(){
 }
 ////////////////////////////////////////////////////////////////////////////////
 function stopVideo(){
-  document.getElementById("video").stop();
+	document.getElementById("video").stop();
 }
 ////////////////////////////////////////////////////////////////////////////////
 /* View in fullscreen */
@@ -233,7 +244,7 @@ function openFullscreen() {
 		screen.orientation.lock('landscape')
 	}
 	// launch fullscreen
-  var video = document.getElementById("videoPlayerContainer");
+	var video = document.getElementById("videoPlayerContainer");
 	if (video.requestFullscreen) {
 		video.requestFullscreen();
 	} else if (video.webkitRequestFullscreen) { /* Safari */
@@ -241,8 +252,8 @@ function openFullscreen() {
 	} else if (video.msRequestFullscreen) { /* IE11 */
 		video.msRequestFullscreen();
 	}
-  document.getElementById("fullscreenButton").style = "display:none;";
-  document.getElementById("exitFullscreenButton").style = "display: inline-block;";
+	document.getElementById("fullscreenButton").style = "display:none;";
+	document.getElementById("exitFullscreenButton").style = "display: inline-block;";
 	return false;
 }
 ////////////////////////////////////////////////////////////////////////////////
@@ -255,25 +266,25 @@ function closeFullscreen() {
 	} else if (document.msExitFullscreen) { /* IE11 */
 		document.msExitFullscreen();
 	}
-  document.getElementById("fullscreenButton").style = "display: inline-block;";
-  document.getElementById("exitFullscreenButton").style = "display: none;";
+	document.getElementById("fullscreenButton").style = "display: inline-block;";
+	document.getElementById("exitFullscreenButton").style = "display: none;";
 	return false;
 }
 ////////////////////////////////////////////////////////////////////////////////
 /*
 //-----------------------------------------------------------------------------
 function startVideoUpdateLoop(){
-  // get the seek bar element
-  var videoPositionBar = document.getElementById("videoPositionBar");
-  var video = document.getElementById("video");
-  // create the event to update the seek bar value
-  videoPositionBar.addEventListener("change", function, videoUpdateLoop(video,videoPositionBar);
+	// get the seek bar element
+	var videoPositionBar = document.getElementById("videoPositionBar");
+	var video = document.getElementById("video");
+	// create the event to update the seek bar value
+	videoPositionBar.addEventListener("change", function, videoUpdateLoop(video,videoPositionBar);
 }
 //-----------------------------------------------------------------------------
 function videoUpdateLoop(var video,var videoPositionBar){
-  var time = (video.duration * (videoPositionBar.value/100));
-  // set new position value
-  video.currentTime = time;
+	var time = (video.duration * (videoPositionBar.value/100));
+	// set new position value
+	video.currentTime = time;
 }
 //-----------------------------------------------------------------------------
 */
