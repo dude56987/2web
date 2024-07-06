bb19c78ac35e25e58a33cf9430ebf73f5c6c9612
dude56987 <dude56987@users.noreply.github.com>
2024-07-05 21:34:35 -0400

    Added javascript zoom in box in comic2web

--- templates/comic_page.php
+++ templates/comic_page.php
@@ -43,6 +43,7 @@
 ?>
 <html id='top' class='comicPageBackground'>
 <head>
+	<meta meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" >
 	<link rel='stylesheet' type='text/css' href='/style.css'>
 	<script src='/2webLib.js'></script>
 	<link rel='icon' type='image/png' href='/favicon.png'>
@@ -89,10 +90,86 @@
 	</style>
 	<script src='/2webLib.js'></script>
 		<script>
+		// build the javascript zoom box that is activated when the picture is left clicked
+		function zoomInBox(idTag, zoom) {
+			var img;
+			var zoomBox;
+			var w;
+			var h;
+			var comicPane;
+			// get the image
+			img = document.getElementById(idTag);
+			// get the distance of
+			comicPane = document.getElementById("comicPane");
+			// comicPane.offsetLeft
+			// create a zoom box container
+			zoomBox = document.createElement("div");
+			zoomBox.setAttribute("id", "zoomBox");
+			zoomBox.setAttribute("class", "zoomBox");
+			// insert the container
+			img.parentElement.insertBefore(zoomBox, img);
+			// setup css inside the zoom box
+			zoomBox.style.backgroundImage = img.style.backgroundImage;
+			zoomBox.style.backgroundRepeat = "no-repeat";
+			// set the background size
+			zoomBox.style.backgroundSize = (img.offsetWidth * zoom) + "px " + (img.offsetHeight * zoom) + "px";
+			//
+			/////////////////////////////////////////////////////////////////////////////////////////
+			// Create functions for the events to call
+			// - Events are setup below the functions
+			/////////////////////////////////////////////////////////////////////////////////////////
+			function readCursorPos(eventObject) {
+				// get the mouse cursor pos
+				var a;
+				var cursorX = 0;
+				var cursorY = 0;
+				eventObject = eventObject || window.event;
+				// get bounds of image
+				a = img.getBoundingClientRect();
+				// get cursor pos
+				cursorX = eventObject.pageX - a.left;
+				cursorY = eventObject.pageY - a.top;
+				// account for scrolling
+				cursorX = cursorX - window.pageXOffset;
+				cursorY = cursorY - window.pageYOffset;
+				// return the x,y pos
+				return {x : cursorX, y : cursorY};
+			}
+			function moveZoomBox(eventObject) {
+				// set the background size so that if the page is zoomed it will adjust
+				zoomBox.style.backgroundSize = (img.offsetWidth * zoom) + "px " + (img.offsetHeight * zoom) + "px";
+				// get the zoom box width and height
+				w = zoomBox.offsetWidth / 2;
+				h = zoomBox.offsetHeight / 2;
+				// move the zoom box position
+				var pos;
+				var x;
+				var y;
+				// stop default events
+				eventObject.preventDefault();
+				// get the current position of the cursor
+				pos = readCursorPos(eventObject);
+				x = pos.x;
+				y = pos.y;
+				// set the zoom box location
+				zoomBox.style.left = ( ( x - w ) ) + "px";
+				zoomBox.style.top = ( ( y - h ) ) + "px";
+				// set the inside of the zoom box
+				zoomBox.style.backgroundPosition = "" + (((x * zoom) - w ) * -1) + "px " + (((y * zoom) - h ) * -1) + "px";
+			}
+			////////////////////////////////////////////////////////////////////////////////
+			// create events for activating the zoom for mouse and touchscreens
+			////////////////////////////////////////////////////////////////////////////////
+			zoomBox.addEventListener("mousemove", moveZoomBox);
+			img.addEventListener("mousemove", moveZoomBox);
+			zoomBox.addEventListener("touchmove", moveZoomBox);
+			img.addEventListener("touchmove", moveZoomBox);
+		}
 		// setup keyboard controls
 		function setupKeys() {
 			document.body.addEventListener('keydown', function(event){
 				const key = event.key;
+				console.log(key);
 				switch (key){
 					case 'ArrowLeft':
 					event.preventDefault();
@@ -154,8 +231,8 @@
 		$comicThumbType="comicThumbPane";
 	}
 
-	echo "<body onload='setupKeys();'>\n";
-	echo "<div id='comicPane' class='$comicPaneType' style='background: ".'url("'.$page.'-thumb.png")'."'>\n";
+	echo "<body id='body' onload='setupKeys();'>\n";
+	echo "<div id='comicPane' class='$comicPaneType zoomBoxContainer' style='background: ".'url("'.$page.'-thumb.png")'."'>\n";
 	echo "<div id='comicThumbPane' class='$comicThumbType' style='background: ".'url("'.$page.'.jpg")'."'>\n";
 	echo "	<a href='$lastPage' class='comicPageButton comicPageButtonLeft left'>\n";
 	echo "		&#8617;\n";
@@ -193,6 +270,49 @@
 	echo "<span id='bottom'></span>\n";
 ?>
 	<script>
+		// load the zoomBox
+		zoomInBox("comicThumbPane",2);
+		// value to tell if the zoomBox should be enabled by a mouse down event
+		var zoomBoxEnabled = true;
+		// activate the zoom if the mouse is clicked and no buttons are active
+		document.body.onmousedown = function(){
+			console.log("mouseDown");
+			if(zoomBoxEnabled){
+				console.log("zoom is enabled");
+				// turn on the zoom
+				//document.getElementById("zoomBox").style.display="inline";
+				document.getElementById("zoomBox").style.visibility="visible";
+				// hide the buttons while the zoom is active
+				document.querySelectorAll(".comicPageButton, #comicPagePopup").forEach(button => {
+					button.style.display="none";
+				});
+			}else{
+				console.log("zoom is disabled");
+			}
+		}
+		// create events for all the buttons to disable using the zoom when the buttons are hovered
+		document.querySelectorAll(".comicPageButton, #comicPagePopup").forEach(button => {
+			button.onmouseenter = function(){
+				console.log("button mouseEnter");
+				zoomBoxEnabled = false;
+				document.getElementById("zoomBox").style.display="none";
+			}
+			button.onmouseleave = function(){
+				console.log("button mouseLeave");
+				zoomBoxEnabled = true;
+				document.getElementById("zoomBox").style.display="inline";
+			}
+		});
+		// reshow the buttons when the zoom is disabled
+		document.body.onmouseup = function(){
+			console.log("mouseUp");
+			//document.getElementById("zoomBox").style.display="none";
+			document.getElementById("zoomBox").style.visibility="hidden";
+			// unhide the buttons
+			document.querySelectorAll(".comicPageButton, #comicPagePopup").forEach(button => {
+				button.style.display="inline";
+			});
+		}
 		// show the progress popup with javascript when the page is loaded, then hide it again
 		function reHidePopup(){
 			console.log("Re hiding the popup")
