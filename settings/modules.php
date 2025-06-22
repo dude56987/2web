16d09b76afe00ce159153c58dd26e5438110b084
dude56987 <dude56987@users.noreply.github.com>
2025-06-21 21:26:10 -0400

    Improvements to modules section in web settings
    - Added newlines for better output formatting
    - Switched to from .errorBanner to .warningBanner

--- settings/modules.php
+++ settings/modules.php
@@ -66,105 +66,105 @@ foreach($modules as $module){
 <hr>
 <?PHP
 foreach($modules as $module){
-	echo "<div id='".$module."' class='inputCard'>";
-	echo "		<h2>$module Module Status</h2>";
-	echo "			<ul>";
-	echo "				<li>";
-	echo "					Enable or disable the $module module.";
-	echo "				</li>";
+	echo "<div id='".$module."' class='inputCard'>\n";
+	echo "	<h2>$module Module Status</h2>\n";
+	echo "	<ul>\n";
+	echo "		<li>\n";
+	echo "			Enable or disable the $module module.\n";
+	echo "		</li>\n";
 	# check the current module and show proper description
 	if ($module == "nfo2web"){
-		echo "				<li>";
-		echo "					Will enable On Demand Video Processing.";
-		echo "				</li>";
-		echo "				<li>";
-		echo "						Enable adding local nfo libaries to the website.";
-		echo "				</li>";
+		echo "		<li>\n";
+		echo "			Will enable On Demand Video Processing.\n";
+		echo "		</li>\n";
+		echo "		<li>\n";
+		echo "			Enable adding local nfo libaries to the website.\n";
+		echo "		</li>\n";
 	}elseif ($module == "ytdl2nfo"){
-		echo "				<li>";
-		echo "						Enable or disable downloading metadata from websites hosting video.";
-		echo "				</li>";
+		echo "		<li>\n";
+		echo "			Enable or disable downloading metadata from websites hosting video.\n";
+		echo "		</li>\n";
 	}elseif ($module == "iptv2web"){
-		echo "				<li>";
-		echo "					Will enable IPTV live channel and IPTV live radio Processing.";
-		echo "				</li>";
+		echo "		<li>\n";
+		echo "			Will enable IPTV live channel and IPTV live radio Processing.\n";
+		echo "		</li>\n";
 	}elseif ($module == "comic2web"){
-		echo "				<li>";
-		echo "					Will enable comic and book Processing.";
-		echo "				</li>";
-		echo "				<li>";
-		echo "					Enable or disable comics section of the website.";
-		echo "				</li>";
+		echo "		<li>\n";
+		echo "			Will enable comic and book Processing.\n";
+		echo "		</li>\n";
+		echo "		<li>\n";
+		echo "			Enable or disable comics section of the website.\n";
+		echo "		</li>\n";
 	}elseif ($module == "weather2web"){
-		echo "				<li>";
-		echo "					Will enable Weather Station Processing.";
-		echo "				</li>";
-		echo "				<li>";
-		echo "					Enable or disable weather on the website.";
-		echo "				</li>";
+		echo "		<li>\n";
+		echo "			Will enable Weather Station Processing.\n";
+		echo "		</li>\n";
+		echo "		<li>\n";
+		echo "			Enable or disable weather on the website.\n";
+		echo "		</li>\n";
 	}elseif ($module == "music2web"){
-		echo "				<li>";
-		echo "					Will enable music Processing.";
-		echo "				</li>";
-		echo "				<li>";
-		echo "					Enable or disable music on the website.";
-		echo "				</li>";
+		echo "		<li>\n";
+		echo "			Will enable music Processing.\n";
+		echo "		</li>\n";
+		echo "		<li>\n";
+		echo "			Enable or disable music on the website.\n";
+		echo "		</li>\n";
 	}elseif ($module == "graph2web"){
-		echo "				<li>";
-		echo "					Will enable graph Processing.";
-		echo "				</li>";
-		echo "				<li>";
-		echo "					Enable or disable the graphs on the website.";
-		echo "				</li>";
+		echo "		<li>\n";
+		echo "			Will enable graph Processing.\n";
+		echo "		</li>\n";
+		echo "		<li>\n";
+		echo "			Enable or disable the graphs on the website.\n";
+		echo "		</li>\n";
 	}elseif ($module == "kodi2web"){
-		echo "				<li>";
-		echo "					Enable or disable sync of linked kodi instances.";
-		echo "				</li>";
+		echo "		<li>\n";
+		echo "			Enable or disable sync of linked kodi instances.\n";
+		echo "		</li>\n";
 	}elseif ($module == "wiki2web"){
-		echo "				<div class='errorBanner'>";
-		echo "					 WARNING: This module is under development and still 🫨 UNSTABLE. Some services and functions of the module may not function completely or correctly.";
-		echo "				</div>";
+		echo "		<div class='warningBanner'>\n";
+		echo "			 WARNING: This module is under development and still 🫨 UNSTABLE. Some services and functions of the module may not function completely or correctly.\n";
+		echo "		</div>\n";
 		# if zimdump does not exist zim files can not be extracted correctly
 		if (! is_file("/usr/bin/zimdump")){
-			echo "					<span class='disabledSetting'>wiki2web REQUIRES zimdump from zim-tools package to extract .zim files<span>";
-			echo "				</li>";
-			echo "				<li>";
-			echo "					<span class='disabledSetting'>Install zim-tools or wiki2web does nothing<span>";
-			echo "				</li>";
+			echo "			<span class='disabledSetting'>wiki2web REQUIRES zimdump from zim-tools package to extract .zim files<span>\n";
+			echo "		</li>\n";
+			echo "		<li>\n";
+			echo "			<span class='disabledSetting'>Install zim-tools or wiki2web does nothing<span>\n";
+			echo "		</li>\n";
 		}
-		echo "				<li>";
-		echo "					Enable or disable extraction of .zim files in wiki directory to the website.";
-		echo "				</li>";
+		echo "		<li>\n";
+		echo "			Enable or disable extraction of .zim files in wiki directory to the website.\n";
+		echo "		</li>\n";
 	}elseif ($module == "git2web"){
-		echo "				<li>";
-		echo "					Will enable <a href='https://wikipedia.org/wiki/Git'>git</a> repo processing.";
-		echo "				</li>";
+		echo "		<li>\n";
+		echo "			Will enable <a href='https://wikipedia.org/wiki/Git'>git</a> repo processing.\n";
+		echo "		</li>\n";
 	}elseif ($module == "ai2web"){
-		echo "				<div class='errorBanner'>";
-		echo "					 WARNING: This module is under development and still 🫨 UNSTABLE. Some services and functions of the module may not function completely or correctly.";
-		echo "				</div>";
-		echo "				<li>";
-		echo "					Will enable machine learning for recommending videos.";
-		echo "				</li>";
-		echo "				<li>";
-		echo "					Will enable diffusion based image generation from text.";
-		echo "				</li>";
-		echo "				<li>";
-		echo "					Will enable gpt4all web interface for prompting.";
-		echo "				</li>";
+		echo "		<div class='warningBanner'>\n";
+		echo "			 WARNING: This module is under development and still 🫨 UNSTABLE. Some services and functions of the module may not function completely or correctly.\n";
+		echo "		</div>\n";
+		echo "		<li>\n";
+		echo "			Will enable machine learning for recommending videos.\n";
+		echo "		</li>\n";
+		echo "		<li>\n";
+		echo "			Will enable diffusion based image generation from text.\n";
+		echo "		</li>\n";
+		echo "		<li>\n";
+		echo "			Will enable gpt4all web interface for prompting.\n";
+		echo "		</li>\n";
 	}
 	# check the module status for drawing enabled or disabled onscreen
 	if (checkModStatus($module)){
-		echo "				<li>";
-		echo "					Currently this module is <span class='enabledSetting'>Enabled</span>.";
-		echo "				</li>";
+		echo "		<li>\n";
+		echo "			Currently this module is <span class='enabledSetting'>Enabled</span>.\n";
+		echo "		</li>\n";
 	}else{
-		echo "				<li>";
-		echo "					Currently this module is <span class='disabledSetting'>Disabled<span>.";
-		echo "				</li>";
+		echo "		<li>\n";
+		echo "			Currently this module is <span class='disabledSetting'>Disabled<span>.\n";
+		echo "		</li>\n";
 	}
-	echo "			</ul>";
-	echo "	<div class='listCard'>";
+	echo "			</ul>\n";
+	echo "	<div class='listCard'>\n";
 	// check the status of the module for the dropdown
 	if (checkModStatus($module)){
 		echo "		<form action='admin.php' class='buttonForm' method='post'>\n";
@@ -184,8 +184,8 @@ foreach($modules as $module){
 	echo "			<button class='button' type='submit' name='".$module."_nuke' value='yes'>☢️ Nuke</button>\n";
 	echo "		</form>\n";
 
-	echo "	</div>";
-	echo "</div>";
+	echo "	</div>\n";
+	echo "</div>\n";
 }
 ?>
 <hr>
