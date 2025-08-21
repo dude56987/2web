6b468e101db3a86ed80c93be07a040ecb3a519a6
dude56987 <dude56987@users.noreply.github.com>
2025-08-21 16:51:54 -0400

    Added IP resolution for exit.php links

--- templates/exit.php
+++ templates/exit.php
@@ -37,7 +37,13 @@ include("/var/cache/2web/web/header.php");
 ################################################################################
 $webDirectory=$_SERVER["DOCUMENT_ROOT"];
 ################################################################################
-if (array_key_exists("to",$_GET)){
+if (array_key_exists("to",$_GET) or array_key_exists("toip",$_GET)){
+	# build the link to the extrnal link
+	if( array_key_exists("to",$_GET) ){
+		$link=urldecode($_GET["to"]);
+	}else if( array_key_exists("toip",$_GET) ){
+		$link=urldecode($_GET["toip"]);
+	}
 	echo "<div class='titleCard'>\n";
 	echo "	<h2>External Redirect</h2>\n";
 	echo "	<ul>\n";
@@ -48,10 +54,53 @@ if (array_key_exists("to",$_GET)){
 	echo "			Click the below link to proceed.\n";
 	echo "		</li>\n";
 	echo "	</ul>\n";
-	# build the link to the extrnal link
-	echo "	<div class='listCard'>\n";
-	echo "		<a class='button' target='_BLANK' rel='noreferer' href='".urldecode($_GET['to'])."'>🌐 ".urldecode($_GET['to'])."</a>\n";
-	echo "	</div>\n";
+	# check if the user is accessing this server from a IP address
+	if(preg_match("/[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}/",$_SERVER["HTTP_HOST"])){
+		if(stripos($link,".local") !== false){
+			$is_ip_user=true;
+		}else{
+			$is_ip_user=false;
+		}
+	}else{
+		$is_ip_user=false;
+	}
+	if( array_key_exists("to",$_GET) and ($is_ip_user == false) ){
+		#
+		echo "	<div class='listCard'>\n";
+		echo "		<a class='button' target='_BLANK' rel='noreferer' href='".$link."'>🌐 ".$link."</a>\n";
+		echo "	</div>\n";
+		# draw the ip address based output
+		echo "	<div class='listCard'>\n";
+		echo "		<a class='button' href='/exit.php?toip=".$link."'>🔢 Resolve IP Address Directly</a>\n";
+		echo "	</div>\n";
+	}else if( array_key_exists("toip",$_GET) or ($is_ip_user == true) ){
+		#echo "<h2>link</h2><pre>".var_export($link,true)."</pre>";
+		# figure out the domain name
+		$linkDomain=str_replace("https://","",$link);
+		$linkDomain=str_replace("http://","",$linkDomain);
+		#echo "<h2>linkDomain1</h2><pre>".var_export($linkDomain,true)."</pre>";
+		$linkDomain=explode("/",$linkDomain);
+		#echo "<h2>linkDomain2</h2><pre>".var_export($linkDomain,true)."</pre>";
+		$linkDomain=$linkDomain[0];
+		#echo "<h2>linkDomain3</h2><pre>".var_export($linkDomain,true)."</pre>";
+		$linkDomain=explode(":",$linkDomain);
+		#echo "<h2>linkDomain4</h2><pre>".var_export($linkDomain,true)."</pre>";
+		$linkDomain=$linkDomain[0];
+		#echo "<h2>linkDomain5</h2><pre>".var_export($linkDomain,true)."</pre>";
+		# get the ip of the domain with DNS
+		$domainIp=gethostbyname($linkDomain);
+		#echo "<h2>domainIp</h2><pre>".var_export($domainIp,true)."</pre>";
+		# replace the domain name with the ip address in the link
+		$linkIp=str_replace($linkDomain,$domainIp,$link);
+		# draw the ip address based output
+		echo "	<div class='listCard'>\n";
+		echo "		<a class='button' target='_BLANK' rel='noreferer' href='".$linkIp."'>🌐 ".$linkIp."</a>\n";
+		echo "	</div>\n";
+		# also draw the redirect button without the ip
+		echo "	<div class='listCard'>\n";
+		echo "		<a class='button' target='_BLANK' rel='noreferer' href='".$link."'>🌐 ".$link."</a>\n";
+		echo "	</div>\n";
+	}
 	echo "	<ul>\n";
 	echo "		<li>\n";
 	echo "			Or you can return to the last page with the button below.\n";
