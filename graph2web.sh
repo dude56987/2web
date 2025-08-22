506cbe94302ce2788fc5d70d306429c1c49725cc
dude56987 <dude56987@users.noreply.github.com>
2025-08-22 14:08:11 -0400

    Improvements and updates to graph2web.sh
    - Removed use of Main()
    - Updated to use addToIndex()
    - Added more output during processing
    - Removed duplicates of cacheCheck(), cleanText(), and INFO()
    - Switched paths to use kodiRoot()
    - Added --mute, --fast, and --parallel options
    - Removed unused --webgen option and function

--- graph2web.sh
+++ graph2web.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 ########################################################################
 # graph2web creates a web interface to munin graphs on 2web server
-# Copyright (C) 2024  Carl J Smith
+# Copyright (C) 2025  Carl J Smith
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -16,8 +16,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ################################################################################
-# enable debug log
-#set -x
 source /var/lib/2web/common
 ################################################################################
 function buildVnstatDevice(){
@@ -64,10 +62,11 @@ function vnstatGen(){
 function update(){
 	# this will launch a processing queue that downloads updates to graph
 	webDirectory=$(webRoot)
+	kodiDirectory=$(kodiRoot)
 	checkModStatus "graph2web"
 
 	createDir "$webDirectory/graphs/"
-	createDir "$webDirectory/kodi/graphs/"
+	createDir "$kodiDirectory/graphs/"
 	# set ownership of munin plugin directory to be able to edit them in the web interface
 	chown -R www-data:www-data /etc/munin/plugins/
 
@@ -85,14 +84,15 @@ function update(){
 		fileName=$(echo "$graphPath" | rev | cut -d'/' -f1 | rev | cut -d'.' -f1 | sed "s/-day//g")
 		if test -f "$webDirectory/graphs/$fileName/index.php";then
 			# if the graph has been added to the server already skip this
-			INFO "Already processed $fileName graph..."
+			INFO "Already processed '$fileName' graph..."
 		else
+			ALERT "Adding '$fileName' graph..."
 			# copy over the metadata for the template to use
 			titleClean=$(echo "$fileName" | sed "s/_/ /g")
 
 			# the graph does not exist on the webserver but is enabled in munin so activate it
 			createDir "$webDirectory/graphs/$fileName/"
-			createDir "$webDirectory/kodi/graphs/$fileName/"
+			createDir "$kodiDirectory/graphs/$fileName/"
 
 			# add the title config file for the php page
 			echo "$titleClean" > "$webDirectory/graphs/$fileName/title.cfg"
@@ -107,13 +107,13 @@ function update(){
 			linkFile "$searchPath/$fileName-year.png" "$webDirectory/graphs/$fileName/year.png"
 
 			# creates kodi directory links
-			linkFile "$searchPath/$fileName-top.png" "$webDirectory/kodi/graphs/$fileName/top.png"
-			linkFile "$searchPath/$fileName-summary.png" "$webDirectory/kodi/graphs/$fileName/summary.png"
-			linkFile "$searchPath/$fileName-hour.png" "$webDirectory/kodi/graphs/$fileName/hour.png"
-			linkFile "$searchPath/$fileName-day.png" "$webDirectory/kodi/graphs/$fileName/day.png"
-			linkFile "$searchPath/$fileName-week.png" "$webDirectory/kodi/graphs/$fileName/week.png"
-			linkFile "$searchPath/$fileName-month.png" "$webDirectory/kodi/graphs/$fileName/month.png"
-			linkFile "$searchPath/$fileName-year.png" "$webDirectory/kodi/graphs/$fileName/year.png"
+			linkFile "$searchPath/$fileName-top.png" "$kodiDirectory/graphs/$fileName/top.png"
+			linkFile "$searchPath/$fileName-summary.png" "$kodiDirectory/graphs/$fileName/summary.png"
+			linkFile "$searchPath/$fileName-hour.png" "$kodiDirectory/graphs/$fileName/hour.png"
+			linkFile "$searchPath/$fileName-day.png" "$kodiDirectory/graphs/$fileName/day.png"
+			linkFile "$searchPath/$fileName-week.png" "$kodiDirectory/graphs/$fileName/week.png"
+			linkFile "$searchPath/$fileName-month.png" "$kodiDirectory/graphs/$fileName/month.png"
+			linkFile "$searchPath/$fileName-year.png" "$kodiDirectory/graphs/$fileName/year.png"
 
 			# build the index entry for the graph on the main index
 			{
@@ -135,15 +135,15 @@ function update(){
 			linkFile "/usr/share/2web/templates/graph.php" "$webDirectory/graphs/$fileName/index.php"
 
 			# add graphs to graphs.index
-			echo "$webDirectory/graphs/$fileName/graphs.index" >> "$webDirectory/graphs/graphs.index"
+			addToIndex "$webDirectory/graphs/$fileName/graphs.index" "$webDirectory/graphs/graphs.index"
 
 			# add to new indexes
-			echo "$webDirectory/graphs/$fileName/graphs.index" >> "$webDirectory/new/graphs.index"
-			echo "$webDirectory/graphs/$fileName/graphs.index" >> "$webDirectory/new/all.index"
+			addToIndex "$webDirectory/graphs/$fileName/graphs.index" "$webDirectory/new/graphs.index"
+			addToIndex "$webDirectory/graphs/$fileName/graphs.index" "$webDirectory/new/all.index"
 
 			# add to new indexes
-			echo "$webDirectory/graphs/$fileName/graphs.index" >> "$webDirectory/random/graphs.index"
-			echo "$webDirectory/graphs/$fileName/graphs.index" >> "$webDirectory/random/all.index"
+			addToIndex "$webDirectory/graphs/$fileName/graphs.index" "$webDirectory/random/graphs.index"
+			addToIndex "$webDirectory/graphs/$fileName/graphs.index" "$webDirectory/random/all.index"
 		fi
 	done
 	###################################################################################################
@@ -160,17 +160,18 @@ function update(){
 			if ! test -f "/etc/munin/plugins/$fileName";then
 				# the plugin is disabled remove it from the web interface
 				rm -rv "$webDirectory/graphs/$fileName/"
-				rm -rv "$webDirectory/kodi/graphs/$fileName/"
+				rm -rv "$kodiDirectory/graphs/$fileName/"
 			fi
 			# if the graph has been added to the server already skip this
 			INFO "Already processed $fileName graph..."
 		else
 			# the graph does not exist on the webserver
 			if test -f "/etc/munin/plugins/$fileName";then
+				ALERT "Adding '$fileName' graph..."
 				# if the graph is enabled in munin
 
 				createDir "$webDirectory/graphs/$fileName/"
-				createDir "$webDirectory/kodi/graphs/$fileName/"
+				createDir "$kodiDirectory/graphs/$fileName/"
 				# read each graph path and build a directory for each set of graphs
 				linkFile "$searchPath/$fileName-day.png" "$webDirectory/graphs/$fileName/day.png"
 				linkFile "$searchPath/$fileName-week.png" "$webDirectory/graphs/$fileName/week.png"
@@ -178,10 +179,10 @@ function update(){
 				linkFile "$searchPath/$fileName-year.png" "$webDirectory/graphs/$fileName/year.png"
 
 				# add kodi directory links
-				linkFile "$searchPath/$fileName-day.png" "$webDirectory/kodi/graphs/$fileName/day.png"
-				linkFile "$searchPath/$fileName-week.png" "$webDirectory/kodi/graphs/$fileName/week.png"
-				linkFile "$searchPath/$fileName-month.png" "$webDirectory/kodi/graphs/$fileName/month.png"
-				linkFile "$searchPath/$fileName-year.png" "$webDirectory/kodi/graphs/$fileName/year.png"
+				linkFile "$searchPath/$fileName-day.png" "$kodiDirectory/graphs/$fileName/day.png"
+				linkFile "$searchPath/$fileName-week.png" "$kodiDirectory/graphs/$fileName/week.png"
+				linkFile "$searchPath/$fileName-month.png" "$kodiDirectory/graphs/$fileName/month.png"
+				linkFile "$searchPath/$fileName-year.png" "$kodiDirectory/graphs/$fileName/year.png"
 
 				# copy over the metadata for the template to use
 				titleClean=$(echo "$fileName" | sed "s/_/ /g" )
@@ -204,15 +205,17 @@ function update(){
 				linkFile "/usr/share/2web/templates/graph.php" "$webDirectory/graphs/$fileName/index.php"
 
 				# add graphs to graphs.index
-				echo "$webDirectory/graphs/$fileName/graphs.index" >> "$webDirectory/graphs/graphs.index"
+				addToIndex "$webDirectory/graphs/$fileName/graphs.index" "$webDirectory/graphs/graphs.index"
 
 				# add to new indexes
-				echo "$webDirectory/graphs/$fileName/graphs.index" >> "$webDirectory/new/graphs.index"
-				echo "$webDirectory/graphs/$fileName/graphs.index" >> "$webDirectory/new/all.index"
+				addToIndex "$webDirectory/graphs/$fileName/graphs.index" "$webDirectory/new/graphs.index"
+				addToIndex "$webDirectory/graphs/$fileName/graphs.index" "$webDirectory/new/all.index"
 
 				# add to new indexes
-				echo "$webDirectory/graphs/$fileName/graphs.index" >> "$webDirectory/random/graphs.index"
-				echo "$webDirectory/graphs/$fileName/graphs.index" >> "$webDirectory/random/all.index"
+				addToIndex "$webDirectory/graphs/$fileName/graphs.index" "$webDirectory/random/graphs.index"
+				addToIndex "$webDirectory/graphs/$fileName/graphs.index" "$webDirectory/random/all.index"
+			else
+				ALERT "Adding '$fileName' does not exist..."
 			fi
 		fi
 	done
@@ -240,15 +243,6 @@ function update(){
 	fi
 }
 ################################################################################
-cleanText(){
-	# remove punctuation from text, remove leading whitespace, and double spaces
-	if [ -f /usr/bin/inline-detox ];then
-		echo "$1" | inline-detox --remove-trailing | sed "s/_/ /g" | tr -d '#'
-	else
-		echo "$1" | sed "s/[[:punct:]]//g" | sed -e "s/^[ \t]*//g" | sed "s/\ \ / /g"
-	fi
-}
-################################################################################
 getJson(){
 	# load the json string value
 	jsonData=$1
@@ -263,120 +257,69 @@ getJson(){
 	return 0
 }
 ################################################################################
-webUpdate(){
-	webDirectory=$(webRoot)
-	#downloadDirectory="$(downloadDir)"
-
-	# run the regular update
-	update
-}
-################################################################################
 function resetCache(){
 	webDirectory=$(webRoot)
+	kodiDirectory=$(kodiRoot)
 	# remove web cached graph data
 	rm -rv "$webDirectory"/graphs/*/
 	rm -v "$webDirectory"/graphs/graphs.index
 	exit
 }
 ################################################################################
-INFO(){
-	width=$(tput cols)
-	# cut the line to make it fit on one line using ncurses tput command
-	buffer="                                                                                "
-	# - add the buffer to the end of the line and cut to terminal width
-	#   - this will overwrite any previous text wrote to the line
-	#   - cut one off the width in order to make space for the \r
-	output="$(echo -n "[INFO]: $1$buffer" | tail -n 1 | cut -b"1-$(( $width - 1 ))" )"
-	# print the line
-	printf "$output\r"
-}
-################################################################################
-function cacheCheck(){
-
-	filePath="$1"
-	cacheDays="$2"
-
-	fileName=$( echo "$filePath" | rev | cut -d'/' -f'1'| rev )
-	fileDir=$( echo "$filePath" | rev | cut -d'/' -f'2-'| rev )
-
-	# return true if cached needs updated
-	if test -f "$filePath";then
-		# check the file date
-		fileFound=$(find "$fileDir" -type f -name "$fileName" -mtime "+$cacheDays" | wc -l)
-		if [ "$fileFound" -gt 0 ] ;then
-			# the file is more than "$cacheDays" days old, it needs updated
-			#INFO "File is to old, update the file $1"
-			return 0
-		else
-			# the file exists and is not old enough in cache to be updated
-			#INFO "File in cache, do not update $1"
-			return 1
-		fi
-	else
-		# the file does not exist, it needs created
-		#INFO "File does not exist, it must be created $1"
-		return 0
-	fi
-}
-################################################################################
 function nuke(){
 	webDirectory=$(webRoot)
+	kodiDirectory=$(kodiRoot)
 	# remove the kodi and web graph files
-	rm -rv "$webDirectory/graphs/" || echo "No files found in graph web directory..."
-	rm -rv "$webDirectory/kodi/graphs/" || echo "No files found in graph web directory..."
-	rm -rv "$webDirectory/new/graphs.index" || echo "No graph index..."
-	rm -rv "$webDirectory/random/graphs.index" || echo "No graph index..."
-	rm -v  "$webDirectory/web_cache/widget_random_graphs.index"
-	rm -v  "$webDirectory/web_cache/widget_new_graphs.index"
-	rm -v  "$webDirectory/activityGraph.png"
-	# remove graphs generated by graph2web
-	rm -rv "/var/cache/2web/generated/graphs/"
-}
-################################################################################
-main(){
-	# set the theme of the lines in CLI output
-	LINE_THEME="papersRand"
+	delete "$kodiDirectory/graphs/"
 	#
-	if [ "$1" == "-w" ] || [ "$1" == "--webgen" ] || [ "$1" == "webgen" ] ;then
-		checkModStatus "graph2web"
-		lockProc "graph2web"
-		webUpdate
-	elif [ "$1" == "-u" ] || [ "$1" == "--update" ] || [ "$1" == "update" ] ;then
-		checkModStatus "graph2web"
-		lockProc "graph2web"
-		update
-	elif [ "$1" == "-r" ] || [ "$1" == "--reset" ] || [ "$1" == "reset" ] ;then
-		lockProc "graph2web"
-		resetCache
-	elif [ "$1" == "-n" ] || [ "$1" == "--nuke" ] || [ "$1" == "nuke" ] ;then
-		lockProc "graph2web"
-		# remove the kodi and web graph files
-		nuke
-	elif [ "$1" == "-h" ] || [ "$1" == "--help" ] || [ "$1" == "help" ] ;then
-		cat "/usr/share/2web/help/graph2web.txt"
-	elif [ "$1" == "-e" ] || [ "$1" == "--enable" ] || [ "$1" == "enable" ] ;then
-		enableMod "graph2web"
-	elif [ "$1" == "-d" ] || [ "$1" == "--disable" ] || [ "$1" == "disable" ] ;then
-		disableMod "graph2web"
-	elif [ "$1" == "-v" ] || [ "$1" == "--version" ] || [ "$1" == "version" ];then
-		echo -n "Build Date: "
-		cat /usr/share/2web/buildDate.cfg
-		echo -n "graph2web Version: "
-		cat /usr/share/2web/version_graph2web.cfg
-	else
-		checkModStatus "graph2web"
-		lockProc "graph2web"
-		update $@
-		webUpdate $@
-		showServerLinks
-		echo "Module Links"
-		drawLine
-		echo "http://$(hostname).local:80/graphs/"
-		drawLine
-		echo "http://$(hostname).local:80/settings/graphs.php"
-		drawLine
-	fi
+	delete "$webDirectory/graphs/"
+	delete "$webDirectory/new/graphs.index"
+	delete "$webDirectory/random/graphs.index"
+	delete "$webDirectory/web_cache/widget_random_graphs.index"
+	delete "$webDirectory/web_cache/widget_new_graphs.index"
+	delete "$webDirectory/activityGraph.png"
+	# remove graphs generated by graph2web
+	delete "/var/cache/2web/generated/graphs/"
 }
 ################################################################################
-main "$@"
-exit
+# set the theme of the lines in CLI output
+LINE_THEME="papersRand"
+#
+INPUT_OPTIONS="$@"
+PARALLEL_OPTION="$(loadOption "parallel" "$INPUT_OPTIONS")"
+MUTE_OPTION="$(loadOption "mute" "$INPUT_OPTIONS")"
+FAST_OPTION="$(loadOption "fast" "$INPUT_OPTIONS")"
+#
+if [ "$1" == "-u" ] || [ "$1" == "--update" ] || [ "$1" == "update" ] ;then
+	checkModStatus "graph2web"
+	lockProc "graph2web"
+	update
+elif [ "$1" == "-r" ] || [ "$1" == "--reset" ] || [ "$1" == "reset" ] ;then
+	lockProc "graph2web"
+	resetCache
+elif [ "$1" == "-n" ] || [ "$1" == "--nuke" ] || [ "$1" == "nuke" ] ;then
+	lockProc "graph2web"
+	# remove the kodi and web graph files
+	nuke
+elif [ "$1" == "-h" ] || [ "$1" == "--help" ] || [ "$1" == "help" ] ;then
+	cat "/usr/share/2web/help/graph2web.txt"
+elif [ "$1" == "-e" ] || [ "$1" == "--enable" ] || [ "$1" == "enable" ] ;then
+	enableMod "graph2web"
+elif [ "$1" == "-d" ] || [ "$1" == "--disable" ] || [ "$1" == "disable" ] ;then
+	disableMod "graph2web"
+elif [ "$1" == "-v" ] || [ "$1" == "--version" ] || [ "$1" == "version" ];then
+	echo -n "Build Date: "
+	cat /usr/share/2web/buildDate.cfg
+	echo -n "graph2web Version: "
+	cat /usr/share/2web/version_graph2web.cfg
+else
+	checkModStatus "graph2web"
+	lockProc "graph2web"
+	update $@
+	showServerLinks
+	drawSmallHeader "Module Links"
+	drawLine
+	echo "http://$(hostname).local:80/graphs/"
+	echo "http://$(hostname).local:80/settings/graphs.php"
+	drawLine
+fi
