8a3b24e851a8ec3c1d4a54a8faec90ba4bbfb20f
dude56987 <dude56987@users.noreply.github.com>
2026-01-18 07:40:37 -0500

    Updated resolver headers with new license

--- resolvers/ytdl-resolver.php
+++ resolvers/ytdl-resolver.php
@@ -4,22 +4,21 @@ include("/usr/share/2web/2webLib.php");
 requireGroup("resolver");
 ########################################################################
 # 2web resolver for caching and playback of video links using yt-dlp
-# Copyright (C) 2025  Carl J Smith
+# Copyright (C) 2026  Carl J Smith
 #
 # This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
+# it under the terms of the GNU Affero General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# GNU Affero General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
 ########################################################################
-
 // NOTE: Do not write any text to the document, this will break the redirect
 // redirect the given file to the resoved url found with youtube-dl
 ################################################################################
